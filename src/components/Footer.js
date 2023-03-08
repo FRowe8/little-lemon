6ecb197ad23a9324d6fae2b4fd751a06e7f6fe9f
FRowe8 <frankrowe1@hotmail.co.uk>
2023-03-08 20:44:30 +0000

    semantic html setup

---
+++ src/components/Footer.js
@@ -0,0 +1,60 @@
+function Footer() {
+  return (
+    <footer>
+      <img alt="logo"></img>
+      <>
+        <h3>Doormat Navigation</h3>
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/menu">Menu</a>
+          </li>
+          <li>
+            <a href="/reservations">Reservations</a>
+          </li>
+          <li>
+            <a href="/order_online">Order Online</a>
+          </li>
+          <li>
+            <a href="/login">Login</a>
+          </li>
+        </ul>
+      </>
+      <>
+        <h3>Contact</h3>
+        <ul>
+          <li>
+            <a href="/address">Address</a>
+          </li>
+          <li>
+            <a href="/phone_number">Phone Number</a>
+          </li>
+          <li>
+            <a href="/email">Email</a>
+          </li>
+        </ul>
+      </>
+      <>
+        <h3>Social Media Links</h3>
+        <ul>
+          <li>
+            <a href="/address">Address</a>
+          </li>
+          <li>
+            <a href="/phone_number">Phone Number</a>
+          </li>
+          <li>
+            <a href="/email">Email</a>
+          </li>
+        </ul>
+      </>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import logo from '../Logo.svg'
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} width={200} height={200} alt="little lemon logo" />
+    </header>
+  )
+}
+
+export default Header
