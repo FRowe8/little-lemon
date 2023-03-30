21a6be700169d141e07f6fce719c251fd98b8792
FRowe8 <frankrowe1@hotmail.co.uk>
2023-03-30 21:00:55 +0100

    started adding styling to the forms on booking page

--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -75,37 +75,40 @@ function BookingPage() {
             </aside>
           </div>
         </section>
-        <section>
-          <div>
+        <section className="register-section">
+          <div className="register-heading">
             <h2>Sign in to collect Points</h2>
           </div>
-
-          <div>
-            <label>First Name</label>
-            <input></input>
+          <div className="register-fields-group">
+            <div className="register-fields">
+              <label>First Name</label>
+              <input></input>
+            </div>
+            <div className="register-fields">
+              <label>Last Name</label>
+              <input></input>
+            </div>
           </div>
-          <div>
-            <label>Last Name</label>
-            <input></input>
+          <div className="register-fields-group">
+            <div className="register-fields">
+              <label>Phone Number</label>
+              <input></input>
+            </div>
+            <div className="register-fields">
+              <label>Email</label>
+              <input></input>
+            </div>
           </div>
-          <div>
-            <label>Phone Number</label>
-            <input></input>
-          </div>
-          <div>
-            <label>Email</label>
-            <input></input>
-          </div>
-          <div>
+          <div className="register-fields">
             <label>Password</label>
             <input></input>
           </div>
-          <div>
+          <div className="register-fields">
             <label>Special Requests</label>
             <input></input>
           </div>
         </section>
-        <section>
+        <section className="reservation-fields">
           <div>
             <h2>Booking Details</h2>
           </div>
